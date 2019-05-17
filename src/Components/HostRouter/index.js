c340a3e965de2b963639601944da2b9fe7c3ad5b
brendanrichards73 <brendan.richards@btinternet.com>
2019-05-17 11:35:01 +0100

    Added Rounds page to Host

--- src/Components/HostRouter/index.js
+++ src/Components/HostRouter/index.js
@@ -4,50 +4,59 @@ import { Route, Switch } from "react-router-dom";
 import MakeGameRoom from "../../Views/Host/MakeGameRoom";
 import PopulateTeams from "../../Views/Host/PopulateTeams";
 import Question from "../..//Views/Host/Question";
+import RoundCard from "../..//Views/Host/RoundCard";
 
 import Login from "../../Components/Login";
 
 export default function({
-  match,
-  appProps,
-  makeGameRoom,
-  teamOptions,
-  joinedRoom,
-  deleteTeamMember,
-  deleteGameRoom,
-  startGame,
-  gameMessage,
-  serverCounter
+    match,
+    appProps,
+    makeGameRoom,
+    teamOptions,
+    joinedRoom,
+    deleteTeamMember,
+    deleteGameRoom,
+    startGame,
+    gameMessage,
+    serverCounter
 }) {
-  return !appProps.user ? (
-    <Login appProps={appProps} />
-  ) : (
-    <Switch>
-      <Route
-        path={`${match.url}/makeroom`}
-        render={() => <MakeGameRoom makeGameRoom={makeGameRoom} />}
-      />
-      <Route
-        path={`${match.url}/teams`}
-        render={() => (
-          <PopulateTeams
-            startGame={startGame}
-            teamOptions={teamOptions}
-            joinedRoom={joinedRoom}
-            deleteTeamMember={deleteTeamMember}
-            deleteGameRoom={deleteGameRoom}
-          />
-        )}
-      />
-      <Route
-        path={`${match.url}/question`}
-        render={() => (
-          <Question gameMessage={gameMessage} serverCounter={serverCounter} />
-        )}
-      />
+    return !appProps.user ? (
+        <Login appProps={appProps} />
+    ) : (
+        <Switch>
+            <Route
+                path={`${match.url}/makeroom`}
+                render={() => <MakeGameRoom makeGameRoom={makeGameRoom} />}
+            />
+            <Route
+                path={`${match.url}/teams`}
+                render={() => (
+                    <PopulateTeams
+                        startGame={startGame}
+                        teamOptions={teamOptions}
+                        joinedRoom={joinedRoom}
+                        deleteTeamMember={deleteTeamMember}
+                        deleteGameRoom={deleteGameRoom}
+                    />
+                )}
+            />
+            <RoundCard path={`${match.url}/roundcard`} />
+            <Route
+                path={`${match.url}/question`}
+                render={() => (
+                    <Question
+                        gameMessage={gameMessage}
+                        serverCounter={serverCounter}
+                    />
+                )}
+            />
 
-      <Route exact path={match.url} render={() => <div>i am a route</div>} />
-      <Route render={() => <div>componet not found...</div>} />
-    </Switch>
-  );
+            <Route
+                exact
+                path={match.url}
+                render={() => <div>i am a route</div>}
+            />
+            <Route render={() => <div>componet not found...</div>} />
+        </Switch>
+    );
 }
---
+++ src/Views/Host/RoundCard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import RoundCard from "../../../Components/Rounds/index";
+
+export default function() {
+    return (
+        <div>
+            <RoundCard />
+        </div>
+    );
+}
