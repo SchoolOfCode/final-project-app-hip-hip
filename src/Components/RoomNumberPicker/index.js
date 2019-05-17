f554c52cdc9452811679f08d9900d6f2217b72fb
brendanrichards73 <brendan.richards@btinternet.com>
2019-05-17 09:43:11 +0100

    work on branding shine

--- src/Components/RoomNumberPicker/index.js
+++ src/Components/RoomNumberPicker/index.js
@@ -2,52 +2,54 @@ import React, { useState } from "react";
 import css from "./roomnumberpicker.module.css";
 
 export default function({
-  makeGameRoom,
-  setHasMadeRoom,
-  setIsGameReadyToStart
+    makeGameRoom,
+    setHasMadeRoom,
+    setIsGameReadyToStart
 }) {
-  const [numberOfTeams, setNumberOfTeams] = useState(1);
+    const [numberOfTeams, setNumberOfTeams] = useState(1);
 
-  function changeNumberOfTeams(num) {
-    if (num === 1) {
-      if (numberOfTeams < 4) {
-        setNumberOfTeams(numberOfTeams + num);
-      }
-    } else {
-      if (numberOfTeams > 1) {
-        setNumberOfTeams(numberOfTeams + num);
-      }
+    function changeNumberOfTeams(num) {
+        if (num === 1) {
+            if (numberOfTeams < 4) {
+                setNumberOfTeams(numberOfTeams + num);
+            }
+        } else {
+            if (numberOfTeams > 1) {
+                setNumberOfTeams(numberOfTeams + num);
+            }
+        }
     }
-  }
 
-  return (
-    <>
-      <button
-        className={css.chooseNumberOfTeamsLess}
-        onClick={() => {
-          changeNumberOfTeams(-1);
-        }}
-      >
-        -
-      </button>
-      <button
-        className={css.chooseNumberOfTeams}
-        onClick={() => {
-          makeGameRoom(numberOfTeams);
-          setHasMadeRoom(true);
-          setIsGameReadyToStart(true);
-        }}
-      >
-        Make room with {numberOfTeams} teams
-      </button>
-      <button
-        className={css.chooseNumberOfTeamsMore}
-        onClick={() => {
-          changeNumberOfTeams(1);
-        }}
-      >
-        +
-      </button>
-    </>
-  );
+    return (
+        <>
+            <div className={css.buttonContainer}>
+                <button
+                    className={css.chooseNumberOfTeamsLess}
+                    onClick={() => {
+                        changeNumberOfTeams(-1);
+                    }}
+                >
+                    -
+                </button>
+                <button
+                    className={css.chooseNumberOfTeams}
+                    onClick={() => {
+                        makeGameRoom(numberOfTeams);
+                        setHasMadeRoom(true);
+                        setIsGameReadyToStart(true);
+                    }}
+                >
+                    Make room with {numberOfTeams} teams
+                </button>
+                <button
+                    className={css.chooseNumberOfTeamsMore}
+                    onClick={() => {
+                        changeNumberOfTeams(1);
+                    }}
+                >
+                    +
+                </button>
+            </div>
+        </>
+    );
 }
