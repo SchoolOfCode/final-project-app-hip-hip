17a0bce3985e996654b547181f5964de7c6776c6
brendanrichards73 <brendan.richards@btinternet.com>
2019-05-20 14:33:48 +0100

    added score board animation

--- src/Components/HostRouter/index.js
+++ src/Components/HostRouter/index.js
@@ -11,62 +11,71 @@ import AnswerView from "../../Views/Host/AnswerView";
 import Login from "../../Components/Login";
 
 export default function({
-  match,
-  appProps,
-  makeGameRoom,
-  teamOptions,
-  joinedRoom,
-  deleteTeamMember,
-  deleteGameRoom,
-  startGame,
-  gameMessage,
-  serverCounter,
-  roundNumber
+    match,
+    appProps,
+    makeGameRoom,
+    teamOptions,
+    joinedRoom,
+    deleteTeamMember,
+    deleteGameRoom,
+    startGame,
+    gameMessage,
+    serverCounter,
+    roundNumber
 }) {
-  return !appProps.user ? (
-    <Login appProps={appProps} />
-  ) : (
-    <Switch>
-      <Route
-        path={`${match.url}/makeroom`}
-        render={() => <MakeGameRoom makeGameRoom={makeGameRoom} />}
-      />
-      <Route
-        path={`${match.url}/teams`}
-        render={() => (
-          <PopulateTeams
-            startGame={startGame}
-            teamOptions={teamOptions}
-            joinedRoom={joinedRoom}
-            deleteTeamMember={deleteTeamMember}
-            deleteGameRoom={deleteGameRoom}
-          />
-        )}
-      />
-      <Route
-        path={`${match.url}/roundcard`}
-        render={() => (
-          <RoundCard roundNumber={roundNumber} gameMessage={gameMessage} />
-        )}
-      />
+    return !appProps.user ? (
+        <Login appProps={appProps} />
+    ) : (
+        <Switch>
+            <Route
+                path={`${match.url}/makeroom`}
+                render={() => <MakeGameRoom makeGameRoom={makeGameRoom} />}
+            />
+            <Route
+                path={`${match.url}/teams`}
+                render={() => (
+                    <PopulateTeams
+                        startGame={startGame}
+                        teamOptions={teamOptions}
+                        joinedRoom={joinedRoom}
+                        deleteTeamMember={deleteTeamMember}
+                        deleteGameRoom={deleteGameRoom}
+                    />
+                )}
+            />
+            <Route
+                path={`${match.url}/roundcard`}
+                render={() => (
+                    <RoundCard
+                        roundNumber={roundNumber}
+                        gameMessage={gameMessage}
+                    />
+                )}
+            />
 
-      <Route
-        path={`${match.url}/question`}
-        render={() => (
-          <Question gameMessage={gameMessage} serverCounter={serverCounter} />
-        )}
-      />
+            <Route
+                path={`${match.url}/question`}
+                render={() => (
+                    <Question
+                        gameMessage={gameMessage}
+                        serverCounter={serverCounter}
+                    />
+                )}
+            />
 
-      <Route path={`${match.url}/answer`} render={() => <AnswerView />} />
+            <Route path={`${match.url}/answer`} render={() => <AnswerView />} />
 
-      <Route
-        path={`${match.url}/score`}
-        render={() => (
-          <ScoreView teamOptions={teamOptions} joinedRoom={joinedRoom} />
-        )}
-      />
+            <Route
+                path={`${match.url}/score`}
+                render={() => (
+                    <ScoreView
+                        teamOptions={teamOptions}
+                        joinedRoom={joinedRoom}
+                    />
+                )}
+            />
 
-      <Route render={() => <div>componet not found...</div>} />
-    </Switch>
-  );
+            <Route render={() => <div>componet not found...</div>} />
+        </Switch>
+    );
 }
