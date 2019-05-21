962e3cc100559ed4431f2504cd9522efa4043063
Kim McGrath <klmcgrath2@gmail.com>
2019-05-21 10:16:03 +0100

    timer component to host question page

--- src/Views/Host/Question/index.js
+++ src/Views/Host/Question/index.js
@@ -13,7 +13,7 @@ export default function({
       {serverCounter.question === 0 ? (
         <>
           <h2>CORRELATE!</h2>
-          <h2 style={{ fontSize: "50px" }}>{serverCounter.round}</h2>
+          <Timer counter={serverCounter.question} />
         </>
       ) : (
         <h2> {serverCounter.question}</h2>
