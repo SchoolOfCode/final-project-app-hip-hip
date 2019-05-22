3a4bcc77ad6af745119ccd110e1fc2d2790c5bcf
mrbenbot <benlee.thatsme@gmail.com>
2019-05-22 14:40:24 +0100

    started pluggin in end page

--- src/Components/EndPage/index.js
+++ src/Components/EndPage/index.js
@@ -7,30 +7,27 @@ const cssOrder = ["teamOne", "teamTwo", "teamThree", "teamFour"].reverse();
 
 //use startgame functionality
 
-export default function EndPage({ joinedRoom, teamOptions }) {
+export default function({ joinedRoom, teamOptions }) {
   let orderedTeams = teamOptions.sort(
     (team, team2) => joinedRoom.scores[team] - joinedRoom.scores[team2]
   );
 
   return (
-   
-
-    <div className={css.teamScoreContainer}>
-      {orderedTeams.map((team, i) => {
-        return (
-          <p key={i} className={cn(css[cssOrder[i]])}>
-            {team}: {joinedRoom.scores[team]}
-          </p>
-        );
-      })}
-    </div>
-
-    <button className={css.quitButton} onClick={MakeGameRoom()}
-    >QUIT GAME</button>
-    //does putting the function here route you to make game room page/view?
-
-
-<button className={css.startAnotherGameButton} onclick={startGame()}>NEW GAME</button> 
-
+    <>
+      <div className={css.teamScoreContainer}>
+        {orderedTeams.map((team, i) => {
+          return (
+            <p key={i} className={cn(css[cssOrder[i]])}>
+              {team}: {joinedRoom.scores[team]}
+            </p>
+          );
+        })}
+      </div>
+      <button className={css.quitButton}>Start Again With New Players</button>
+      //does putting the function here route you to make game room page/view?
+      <button className={css.startAnotherGameButton} onclick={startGame}>
+        New Game With Same Players
+      </button>
+    </>
   );
 }
