c7b5feb5f6bedccdce0ee9b76e36157f028c826f
mrbenbot <benlee.thatsme@gmail.com>
2019-06-05 10:00:40 +0100

    hook split

--- src/Views/Host/ListOfPlayersJoiningRoom/index.js
+++ src/Views/Host/ListOfPlayersJoiningRoom/index.js
@@ -2,32 +2,32 @@ import React from "react";
 
 import TeamBoxes from "../../../Components/HostTeamJoiningBoxes";
 import CorrelateLogo from "../../../Components/Branding";
-import css from "./PopulateTeams.module.css";
+import css from "./ListOfPlayersJoining.css";
 
 export default function({
-	teamOptions,
-	joinedRoom,
-	deleteTeamMember,
-	deleteGameRoom,
-	startGame
+  teamOptions,
+  joinedRoom,
+  deleteTeamMember,
+  deleteGameRoom,
+  startGame
 }) {
-	return (
-		<main>
-			<CorrelateLogo />
-			<div className={css.roomCodeContainer}>
-				<p className={css.p1}>Room Code:</p>
-				<p className={css.p2}>{joinedRoom.id}</p>
-			</div>
+  return (
+    <main>
+      <CorrelateLogo />
+      <div className={css.roomCodeContainer}>
+        <p className={css.p1}>Room Code:</p>
+        <p className={css.p2}>{joinedRoom.id}</p>
+      </div>
 
-			<button className={css.startGame} onClick={startGame}>
-				Start Game
-			</button>
-			<ul>
-				{joinedRoom.players.map(({ name }) => (
-					<li>{name}</li>
-				))}
-			</ul>
-			<button onClick={deleteGameRoom}>delete room and start again</button>
-		</main>
-	);
+      <button className={css.startGame} onClick={startGame}>
+        Start Game
+      </button>
+      <ul>
+        {joinedRoom.players.map(({ name }) => (
+          <li>{name}</li>
+        ))}
+      </ul>
+      <button onClick={deleteGameRoom}>delete room and start again</button>
+    </main>
+  );
 }
