b38b0bff4f09cd4483458e1c5a64a9ee9013ca28
brendanrichards73 <brendan.richards@btinternet.com>
2019-06-07 14:20:48 +0100

    PhoneScoreBoard + GameMessageView styled css

--- src/Components/PhoneScoreBoard/index.js
+++ src/Components/PhoneScoreBoard/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import css from "./PhoneScoreBoard.module.css";
+import botImageSmall from "../../Components/images/botImageSmall.png";
 
 export default function({ joinedRoom, teamColor, gameMessage }) {
     const style = { backgroundColor: teamColor };
     console.log("joined room in phone score board", joinedRoom);
 
     return (
-        <div className={css.textContainer} style={style}>
+        <div className={css.textContainer}>
+            <img className={css.botImageSmall} src={botImageSmall} alt="bot" />
             <p className={css.phoneMessage}>{gameMessage}</p>
         </div>
     );
--- src/Views/Player/HoldingPageView/index.js
+++ src/Views/Player/HoldingPageView/index.js
@@ -2,19 +2,19 @@ import React from "react";
 import css from "./HoldingPageView.module.css";
 
 export default function({ joinedRoom, teamColor }) {
-  return (
-    <div className={css.textContainer}>
-      <div className={css.text}>
-        <span className={css.yay}> YAY!</span>
-        <br />
-        you are on the
-        <br />
-        <span className={css.teamColor}> {teamColor}</span> team
-        <br />
-        in <span className={css.joinedRoom}>{joinedRoom.id}</span>
-        <br />
-        so just chill whilst everyone else gets their act together.
-      </div>
-    </div>
-  );
+    return (
+        <div className={css.textContainer}>
+            <div className={css.text}>
+                <span className={css.yay}> YAY!</span>
+                <br />
+                You are on the
+                <br />
+                <span className={css.teamColor}> {teamColor}</span> team
+                <br />
+                in <span className={css.joinedRoom}>{joinedRoom.id}</span>
+                <br />
+                so just chill whilst everyone else gets their act together.
+            </div>
+        </div>
+    );
 }
