d2ebfd612103311f745ee62a470c8548c84b5752
mrbenbot <benlee.thatsme@gmail.com>
2019-06-07 14:23:20 +0100

    added more music

--- src/Views/Host/RoundCard/index.js
+++ src/Views/Host/RoundCard/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import RoundCard from "../../../Components/Rounds/index";
+import useAudio from "../../../Hooks/UseAudio";
 
 export default function({ gameMessage, roundNumber }) {
-  return (
-    <div>
-      <RoundCard gameMessage={gameMessage} roundNumber={roundNumber} />
-    </div>
-  );
+	const [playing, toggle] = useAudio(
+		`${process.env.PUBLIC_URL}/music/gong.mp3`
+	);
+	useEffect(() => {
+		toggle();
+	}, []);
+
+	return (
+		<div>
+			<RoundCard gameMessage={gameMessage} roundNumber={roundNumber} />
+		</div>
+	);
 }
