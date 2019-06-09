cde921995b3914f4182496ef23ea80790c3a10f1
mrbenbot <benlee.thatsme@gmail.com>
2019-06-09 17:43:29 +0100

    all good fingers crossed

--- src/Components/JoinTeam/index.js
+++ src/Components/JoinTeam/index.js
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
-import css from "./Jointeam.module.css";
+import css from "./JoinTeam.module.css";
 
 export default function({ setHasJoinedTeam, joinTeam, teamOptions }) {
-  const [name, setName] = useState("");
-  return (
-    <>
-      <input
-        className={css.nameInput}
-        type="text"
-        placeholder="your name here"
-        value={name}
-        onChange={e => setName(e.target.value)}
-      />
-      <br />
-      {teamOptions.map((item, i) => (
-        <button
-          className={css.teamOptions}
-          style={{ backgroundColor: item }}
-          key={i}
-          onClick={() => {
-            if (name.length > 0) {
-              // setHasJoinedTeam(true);
-              joinTeam(item, name);
-            }
-          }}
-        >
-          join the {item} team
-        </button>
-      ))}
-    </>
-  );
+	const [name, setName] = useState("");
+	return (
+		<>
+			<input
+				className={css.nameInput}
+				type="text"
+				placeholder="your name here"
+				value={name}
+				onChange={e => setName(e.target.value)}
+			/>
+			<br />
+			{teamOptions.map((item, i) => (
+				<button
+					className={css.teamOptions}
+					style={{ backgroundColor: item }}
+					key={i}
+					onClick={() => {
+						if (name.length > 0) {
+							// setHasJoinedTeam(true);
+							joinTeam(item, name);
+						}
+					}}
+				>
+					join the {item} team
+				</button>
+			))}
+		</>
+	);
 }
