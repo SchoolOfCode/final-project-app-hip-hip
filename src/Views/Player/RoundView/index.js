cdf5c4761388635ba2c48e7a07a21ead0c351f36
brendanrichards73 <brendan.richards@btinternet.com>
2019-06-09 19:14:56 +0100

    Styled buttons with hover and animation

--- src/Views/Player/RoundView/index.js
+++ src/Views/Player/RoundView/index.js
@@ -2,23 +2,23 @@ import React from "react";
 import css from "./RoundView.module.css";
 
 const RoundCard = ({ gameMessage, roundNumber, questionType }) => {
-	return (
-		<div className={css.container}>
-			<h1 className={css.round}>Round</h1>
-			<h1 className={css.roundNumber}>{roundNumber}</h1>
+    return (
+        <div className={css.container}>
+            <h1 className={css.round}>Round</h1>
+            <h1 className={css.roundNumber}>{roundNumber}</h1>
 
-			<h3 className={css.roundMessage}>{gameMessage}</h3>
-			{questionType === "order" ? (
-				<p className={css.pointsInfo}>
-					each correct answer is worth {roundNumber * 100}
-				</p>
-			) : (
-				<p className={css.pointsInfo}>
-					the correct answer will score you {roundNumber * 100 * 4}
-				</p>
-			)}
-		</div>
-	);
+            <h3 className={css.roundMessage}>{gameMessage}</h3>
+            {questionType === "order" ? (
+                <p className={css.pointsInfo}>
+                    Each correct answer is worth {roundNumber * 100}
+                </p>
+            ) : (
+                <p className={css.pointsInfo}>
+                    The correct answer will score you {roundNumber * 100 * 4}
+                </p>
+            )}
+        </div>
+    );
 };
 
 export default RoundCard;
