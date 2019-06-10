6955ae5dfc4b7389b9caf5f19a5df1611a3b315b
mrbenbot <benlee.thatsme@gmail.com>
2019-06-10 10:48:56 +0100

    traffic lights added

--- src/Views/Host/Question/index.js
+++ src/Views/Host/Question/index.js
@@ -7,56 +7,66 @@ import bot2 from "../../../Components/images/bot2.png";
 import padlock from "./padlock.svg";
 import cn from "classnames";
 
+const joinedRoom = {
+  teamsArray: ["blue"]
+};
+const teamsThatHaveSubmitted = [];
+
 export default function({
-	gameMessage,
-	serverCounter,
-	teamsThatHaveSubmitted,
-	joinedRoom,
-	questionType
+  gameMessage,
+  serverCounter,
+  teamsThatHaveSubmitted,
+  joinedRoom,
+  questionType
 }) {
-	const [playing, toggle] = useAudio(
-		`${process.env.PUBLIC_URL}/music/${
-			questionType === "order" ? "orderRound.mp3" : "pictureRound.mp3"
-		}`
-	);
+  const [playing, toggle] = useAudio(
+    `${process.env.PUBLIC_URL}/music/${
+      questionType === "order" ? "orderRound.mp3" : "pictureRound.mp3"
+    }`
+  );
 
-	// const teamsArray = ["red", "blue"];
-	useEffect(() => {
-		toggle();
-	}, []);
+  // const teamsArray = ["red", "blue"];
+  useEffect(() => {
+    toggle();
+  }, []);
 
-	return (
-		<div>
-			<img className={(css.bot2 = " " + css.bot2After)} src={bot2} alt="bot2" />
-			<h1 className={css.question}>{gameMessage}</h1>
-			{serverCounter.question === 0 ? (
-				<>
-					<h2 className={css.letsCollaborate}>COLLABORATE!</h2>
-					<Timer counter={serverCounter.round} />
-				</>
-			) : (
-				<h2 className={css.letsCollaborate}> {serverCounter.question}</h2>
-			)}
+  return (
+    <div>
+      <img className={(css.bot2 = " " + css.bot2After)} src={bot2} alt="bot2" />
+      <h1 className={css.question}>{gameMessage}</h1>
+      {serverCounter.question === 0 ? (
+        <>
+          <h2 className={css.letsCollaborate}>COLLABORATE!</h2>
+          <Timer counter={serverCounter.round} />
+        </>
+      ) : (
+        <div
+          className={cn(
+            css.trafficlights,
+            css[`light${serverCounter.question}`]
+          )}
+        />
+      )}
 
-			{serverCounter.question === 0 && (
-				<div className={css.teamBoxContainer}>
-					{joinedRoom.teamsArray.map((team, i) => (
-						<div
-							style={{ backgroundColor: team }}
-							className={cn(
-								css.teamBox,
-								!teamsThatHaveSubmitted.some(item => item === team) &&
-									css.wiggle
-							)}
-							key={i}
-						>
-							{teamsThatHaveSubmitted.some(item => item === team) && (
-								<img className={css.padlock} src={padlock} />
-							)}
-						</div>
-					))}
-				</div>
-			)}
-		</div>
-	);
+      {serverCounter.question === 0 && (
+        <div className={css.teamBoxContainer}>
+          {joinedRoom.teamsArray.map((team, i) => (
+            <div
+              style={{ backgroundColor: team }}
+              className={cn(
+                css.teamBox,
+                !teamsThatHaveSubmitted.some(item => item === team) &&
+                  css.wiggle
+              )}
+              key={i}
+            >
+              {teamsThatHaveSubmitted.some(item => item === team) && (
+                <img className={css.padlock} src={padlock} />
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
