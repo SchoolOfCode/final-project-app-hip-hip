6ade93e7c3f3507b9222e8e604f846e63ea8e70f
mrbenbot <benlee.thatsme@gmail.com>
2019-06-10 12:37:51 +0100

    picture answer music

--- src/Views/Host/PictureAnswerView/index.js
+++ src/Views/Host/PictureAnswerView/index.js
@@ -1,13 +1,20 @@
 import React from "react";
-import HostAnswer from "../../../Components/HostAnswer";
+import useAudio from "../../../Hooks/UseAudio";
+
 import css from "./PictureAnswerView.module.css";
 
 export default function({ joinedRoom, gameMessage, pictureUrl }) {
-	return (
-		<div>
-			<h2 className={css.question}>{gameMessage}</h2>
+  const [playing, toggle] = useAudio(
+    `${process.env.PUBLIC_URL}/music/goFindTeams.mp3`
+  );
+  useEffect(() => {
+    toggle();
+  }, []);
+  return (
+    <div>
+      <h2 className={css.question}>{gameMessage}</h2>
 
-			<img className={css.picture} src={pictureUrl} alt="not telling..." />
-		</div>
-	);
+      <img className={css.picture} src={pictureUrl} alt="not telling..." />
+    </div>
+  );
 }
