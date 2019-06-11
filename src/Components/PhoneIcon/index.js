4e70eeaeb865f1f3c2af7d2997b375e165d9938b
mrbenbot <benlee.thatsme@gmail.com>
2019-06-11 11:27:11 +0100

    final changes

--- src/Components/PhoneIcon/index.js
+++ src/Components/PhoneIcon/index.js
@@ -5,17 +5,8 @@ import css from "./PhoneIcon.module.css";
 function PhoneIcon({ name }) {
   return (
     <div className={css.wrapper}>
-      <img
-        className={
-          name.length < 5
-            ? css.phone
-            : name.length > 9
-            ? css.phoneSideBig
-            : css.phoneSideSmall
-        }
-        src={phone}
-      />
-      <p className={name.length < 9 ? css.name : css.nameBig}>{name}</p>
+      <img className={css.phoneSideSmall} src={phone} />
+      <p className={css.name}>{name}</p>
     </div>
   );
 }
